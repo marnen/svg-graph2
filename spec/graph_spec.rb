04d39fc2f7f82b38ddf9314e458df00aa279e280
Marnen Laibow-Koser <marnen@marnen.org>
2020-04-20 14:55:41 -0400

    Add some specs for Graph#add_data. [#25]

--- spec/graph_spec.rb
+++ spec/graph_spec.rb
@@ -17,8 +17,58 @@ describe SVG::Graph::Graph do
     end
   end
 
+  let(:graph) { dummy_graph.new({})}
+
+  describe 'constructor' do
+    subject { graph }
+
+    it { is_expected.to be_a_kind_of described_class }
+  end
+
+  describe '#add_data' do
+    shared_examples 'add_data' do
+      shared_examples 'invalid data' do
+        it 'raises an error' do
+          expect { graph.add_data params }.to raise_error RuntimeError, /^No data provided/
+        end
+      end
+
+      context 'no :data key' do
+        let(:params) { Hash[*Faker::Lorem.words(rand(2..5) * 2)] }
+        include_examples 'invalid data'
+      end
+
+      context ':data key is not an array' do
+        let(:params) { {data: Faker::Lorem.sentence} }
+        include_examples 'invalid data'
+      end
+
+      context 'string "data" key is also invalid' do
+        let(:params) { {'data' => Faker::Lorem.words} }
+        include_examples 'invalid data'
+      end
+
+      context 'valid data: :data key with array' do
+        let(:params) { {data: Faker::Lorem.words} }
+
+        it 'succeeds' do
+          expect { graph.add_data params}.not_to raise_error
+        end
+      end
+    end
+
+    context 'starting empty' do
+      include_examples 'add_data'
+    end
+
+    context 'starting with data already added' do
+      let(:graph) { super().tap {|graph| graph.add_data data: Faker::Lorem.words } }
+      include_examples 'add_data'
+    end
+  end
+
   describe '#to_iruby' do
-    let(:graph) { dummy_graph.new({}).tap {|graph| graph.add_data data: [1, 2, 3] } }
+    let(:graph) { super().tap {|graph| graph.add_data data: [1, 2, 3] } }
 
     it 'returns an HTML MIME type identifier and the SVG content of the graph' do
       expect(graph.to_iruby).to be == ['text/html', graph.burn_svg_only]
