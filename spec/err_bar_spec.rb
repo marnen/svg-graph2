5aaced1e6d0bf64602ed6f59a7fa7d136223c270
Marnen Laibow-Koser <marnen@marnen.org>
2020-04-27 18:56:57 -0400

    Document ErrBar constructor behavior. [#25]

--- spec/err_bar_spec.rb
+++ spec/err_bar_spec.rb
@@ -3,6 +3,52 @@ require 'spec_helper'
 require_relative '../lib/SVG/Graph/ErrBar'
 
 describe SVG::Graph::ErrBar do
+  describe 'constructor' do
+    let(:error_bars) { Faker::Lorem.words number: rand(1..5) }
+    let(:fields) { Faker::Lorem.words number: rand(1..5) }
+    let(:config) { {errorBars: error_bars, fields: fields} }
+
+    subject { described_class.new config }
+
+    shared_examples 'invalid unless non-empty array' do
+      shared_examples 'invalid data' do
+        it 'raises an error' do
+          expect { subject }.to raise_error 'fields was not supplied or is empty'
+        end
+      end
+
+      context 'not present' do
+        let(:config) { super().tap {|config| config.delete field } }
+        include_examples 'invalid data'
+      end
+
+      context 'not an array' do
+        let(:config) { super().merge field => Faker::Lorem.sentence }
+        include_examples 'invalid data'
+      end
+
+      context 'an empty array' do
+        let(:config) { super().merge field => [] }
+        include_examples 'invalid data'
+      end
+    end
+
+    context ':errorBars' do
+      let(:field) { :errorBars }
+      include_examples 'invalid unless non-empty array'
+    end
+
+    context ':fields' do # TODO: this probably belongs in shared examples for BarBase
+      let(:field) { :fields }
+      include_examples 'invalid unless non-empty array'
+    end
+
+    context 'otherwise' do
+      it { is_expected.to be_a_kind_of described_class }
+      it { is_expected.to be_a_kind_of SVG::Graph::BarBase }
+    end
+  end
+
   describe '#burn' do
     it 'returns a basic SVG graph' do
       fields = %w(Jan Feb);
